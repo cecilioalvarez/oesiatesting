4a75d40da78e4536ed6957e96e2d1495eb64a2ec
cecilio <cecilio@DESKTOP-RQLPO9J.lan>
2022-09-13 10:45:11 +0200

    nuevos ejemplos

--- mavenbasico/src/main/java/com/arquitecturajava/proyecto2/Principal8.java
+++ mavenbasico/src/main/java/com/arquitecturajava/proyecto2/Principal8.java
@@ -6,9 +6,20 @@ public class Principal8 {
 		
 		
 		Thread t= new Thread(new TareaCopiar());
-		Thread t2= new Thread(new Thread(new TareaBorrar(t)));
+		Thread t2= new Thread(new TareaBorrar(t));
 		t.start();
 		t2.start();
+		
+		for( int i=0;i<10;i++) {
+			
+			try {
+				Thread.sleep(500);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			System.out.println("hola");
+		}
 
 	}
 
